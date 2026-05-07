5537735039cee74271416bfe4cd2627e90b2878b
Valentin Polikarpov <valpolik@gmail.com>
2026-05-07 22:58:05 +0300

    resources :users

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :users do
+    collection do
+      post 'login'
+    end
+  end
 end
