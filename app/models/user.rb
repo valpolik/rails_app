b83b6cd9355b0aeebf1f9722942dd2f14e6dba5e
Valentin Polikarpov <valpolik@gmail.com>
2026-05-07 22:54:33 +0300

    generates_token_for :authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
 
-  # generates_token_for :authentication, expires_in: 1.year do
-  #   password_digest&.last(4)
-  # end
+  generates_token_for :authentication, expires_in: 1.year do
+    password_digest&.last(4)
+  end
 end
