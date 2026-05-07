08b78eb1457573842d32eb90d9afe967d04136c4
Valentin Polikarpov <valpolik@gmail.com>
2026-05-07 22:44:10 +0300

    class User < ApplicationRecord

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # has_many :posts, dependent: :destroy
+  has_secure_password
+  normalizes :email, with: -> email { email.strip.downcase }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # generates_token_for :authentication, expires_in: 1.year do
+  #   password_digest&.last(4)
+  # end
+end
