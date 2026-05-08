4975ecf35b2a26a4a43b78cca0c9610e777b291a
Valentin Polikarpov <valpolik@gmail.com>
2026-05-08 21:33:47 +0300

    docker compose --file docker-compose-new.yml exec web rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_07_145908) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_08_180103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,4 +30,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_07_145908) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
